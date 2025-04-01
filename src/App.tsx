03cf6bb724e070b011e045320177f5b6198dcc1e
IDahok <contato.dahok@gmail.com>
2025-03-31 23:17:44 -0300

    Featured: Ajuste de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProdutoList } from './components/ProdutoList';
 import { ClienteList } from './components/ClienteList';
 import { VendaList } from './components/VendaList';
@@ -7,27 +7,60 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
+  const [secaoAtiva, setSecaoAtiva] = useState('produtos');
+
+  const renderizarConteudo = () => {
+    switch (secaoAtiva) {
+      case 'produtos':
+        return <ProdutoList />;
+      case 'clientes':
+        return <ClienteList />;
+      case 'vendas':
+        return <VendaList />;
+      case 'relatorio':
+        return <RelatorioVendas />;
+      default:
+        return <ProdutoList />;
+    }
+  };
+
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="#">Sistema de Vendas</a>
+          <a className="navbar-brand" href="#" onClick={(e) => { e.preventDefault(); setSecaoAtiva('produtos'); }}>Sistema de Vendas</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="#produtos">Produtos</a>
+                <a className={`nav-link ${secaoAtiva === 'produtos' ? 'active' : ''}`} 
+                   href="#" 
+                   onClick={(e) => { e.preventDefault(); setSecaoAtiva('produtos'); }}>
+                  Produtos
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#clientes">Clientes</a>
+                <a className={`nav-link ${secaoAtiva === 'clientes' ? 'active' : ''}`} 
+                   href="#" 
+                   onClick={(e) => { e.preventDefault(); setSecaoAtiva('clientes'); }}>
+                  Clientes
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#vendas">Vendas</a>
+                <a className={`nav-link ${secaoAtiva === 'vendas' ? 'active' : ''}`} 
+                   href="#" 
+                   onClick={(e) => { e.preventDefault(); setSecaoAtiva('vendas'); }}>
+                  Vendas
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#relatorio">Relatório</a>
+                <a className={`nav-link ${secaoAtiva === 'relatorio' ? 'active' : ''}`} 
+                   href="#" 
+                   onClick={(e) => { e.preventDefault(); setSecaoAtiva('relatorio'); }}>
+                  Relatório
+                </a>
               </li>
             </ul>
           </div>
@@ -35,18 +68,7 @@ function App() {
       </nav>
 
       <div className="container mt-4">
-        <div id="produtos">
-          <ProdutoList />
-        </div>
-        <div id="clientes" className="mt-5">
-          <ClienteList />
-        </div>
-        <div id="vendas" className="mt-5">
-          <VendaList />
-        </div>
-        <div id="relatorio" className="mt-5">
-          <RelatorioVendas />
-        </div>
+        {renderizarConteudo()}
       </div>
     </div>
   );
